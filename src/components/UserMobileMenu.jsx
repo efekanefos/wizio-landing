a2836210544528cca7738c4063eee59c30dedc79
Efekan EFE <efekanefos@gmail.com>
2025-02-16 15:13:34 +0300

    register_window_added

--- src/components/UserMobileMenu.jsx
+++ src/components/UserMobileMenu.jsx
@@ -15,11 +15,13 @@ import XIcon from "./newIcons/XIcon";
 import FacebookIcon from "./newIcons/FacebookIcon";
 /* Login SVG Components */
 import VisiblePasswordIcon from "./newIcons/VisiblePasswordIcon";
+import UserEditIcon from "./newIcons/UserEditIcon";
+import HiddenPasswordIcon from "./newIcons/HiddenPasswordIcon";
 
 function UserMobileMenu() {
   const [contactStatus, setContactStatus] = useState(false);
   const [languageStatus, setLanguageStatus] = useState(false);
-  const [loginStatus, setLoginStatus] = useState(false);
+  const [loginStatus, setLoginStatus] = useState("button");
   return (
     <div className="bg-white relative z-50 h-screen">
       {/* Menu List */}
@@ -122,15 +124,15 @@ function UserMobileMenu() {
         )}
 
         {/* Login Button */}
-        {!loginStatus ? (
-          <div onClick={() => setLoginStatus(true)} className="flex justify-start items-center gap-4 p-5 border border-gray-200 rounded-full shadow-xl cursor-pointer">
+        {loginStatus === "button" ? (
+          <div onClick={() => setLoginStatus("login")} className="flex justify-start items-center gap-4 p-5 border border-gray-200 rounded-full shadow-xl cursor-pointer">
             <GuestIcon className={"w-6 h-6 fill-white"} />
             <p className="text-[16px] font-semibold text-black">Login</p>
           </div>
-        ) : (
+        ) : loginStatus === "login" ? (
           <div className="flex flex-col border border-gray-200 rounded-[32px] shadow-xl p-3">
             {/* Header Row */}
-            <div onClick={() => setLoginStatus(false)} className="flex justify-between items-center border-b border-gray-200 p-3 py-5 cursor-pointer">
+            <div onClick={() => setLoginStatus("button")} className="flex justify-between items-center border-b border-gray-200 p-3 py-5 cursor-pointer">
               {/* Icon and Title */}
               <div className="flex justify-start items-center gap-4 w-full">
                 <GuestIcon className={"w-6 h-6 fill-white"} />
@@ -168,12 +170,19 @@ function UserMobileMenu() {
                   </div>
                   <span className="text-xs font-light text-gray-300">Remember me</span>
                 </label>
-                <p className="text-xs font-light text-gray-300 cursor-pointer">Forget Password?</p>
+                <button
+                  onClick={() => {
+                    setLoginStatus("password");
+                  }}
+                  className="text-xs font-light text-gray-300 cursor-pointer"
+                >
+                  Forget Password?
+                </button>
               </div>
 
               {/* Buttons */}
               <div className="w-full flex flex-col gap-3 p-3 pb-0">
-                <button onClick={() => navigate("/logged-in-landing-page")} className="bg-black text-sm text-white font-light text-center w-full py-3 rounded-full" type="submit">
+                <button className="bg-black text-sm text-white font-light text-center w-full py-3 rounded-full" type="submit">
                   Login
                 </button>
 
@@ -182,8 +191,7 @@ function UserMobileMenu() {
                   <button
                     className="text-xs font-light text-black"
                     onClick={() => {
-                      setShowLoginWindow(false);
-                      setShowRegisterWindow(true);
+                      setLoginStatus("register");
                     }}
                   >
                     Sign Up
@@ -192,7 +200,112 @@ function UserMobileMenu() {
               </div>
             </div>
           </div>
-        )}
+        ) : loginStatus === "register" ? (
+          <div className="flex flex-col border border-gray-200 rounded-[32px] shadow-xl p-3">
+            {/* Header Row */}
+            <div className="flex justify-between items-center border-b border-gray-200 p-3 py-5">
+              {/* Icon and Title */}
+              <div className="flex justify-start items-center gap-4 w-full">
+                <UserEditIcon className={"w-6 h-6 fill-white"} />
+                <p className="text-2xl text-black font-semibold">Register</p>
+              </div>
+            </div>
+            {/* User Form */}
+            <div className="flex flex-col justify-center items-center mt-3">
+              {/* Username Input */}
+              <div className="w-full flex justify-between items-center">
+                <div className="flex flex-col w-full">
+                  <label className="px-3 pt-3 text-xs text-gray-300 font-light">Username*</label>
+                  <input readOnly value="William Douglas" className="px-3 pb-3 w-full text-base outline-none border-b border-gray-200" type="text" />
+                </div>
+              </div>
+
+              {/* Phone Number Input */}
+              <div className="w-full flex justify-between items-center">
+                <div className="flex flex-col w-full">
+                  <label className="px-3 pt-3 text-xs text-gray-300 font-light">Phone*</label>
+                  <input readOnly value="+44 1202 299 70" className="px-3 pb-3 w-full text-base outline-none border-b border-gray-200" type="tel" />
+                </div>
+              </div>
+
+              {/* Email Input */}
+              <div className="w-full flex justify-between items-center">
+                <div className="flex flex-col w-full">
+                  <label className="px-3 pt-3 text-xs text-gray-300 font-light">E-mail*</label>
+                  <input readOnly value="william_douglas@gmail.com" className="px-3 pb-3 w-full text-base outline-none border-b border-gray-200" type="email" />
+                </div>
+              </div>
+
+              {/* Password Visible Input */}
+              <div className="w-full flex justify-between items-center">
+                <div className="flex flex-col w-full">
+                  <div className="pt-3 flex justify-between items-center">
+                    <label className="px-3 text-xs text-gray-300 font-light">Password</label>
+                    <VisiblePasswordIcon className={"w-4 h-4 fill-current"} />
+                  </div>
+                  <input readOnly value="William324221" className="px-3 pb-3 w-full text-base outline-none border-b border-gray-200" type="text" />
+                </div>
+              </div>
+
+              {/* Password Input */}
+              <div className="w-full flex justify-between items-center">
+                <div className="flex flex-col w-full">
+                  <div className="pt-3 flex justify-between items-center">
+                    <label className="px-3 text-xs text-gray-300 font-light">Confirm password*</label>
+                    <HiddenPasswordIcon className={"w-4 h-4 fill-current"} />
+                  </div>
+                  <input readOnly value="****************" className="px-3 pb-3 w-full text-base outline-none border-b border-gray-200" type="password" />
+                </div>
+              </div>
+
+              {/* Accept Our Terms */}
+              <div className="w-full flex justify-between items-center my-3 px-3">
+                <label htmlFor="rememberme" className="flex justify-start items-center gap-3 cursor-pointer">
+                  <input id="rememberme" type="checkbox" className="peer hidden" />
+                  <div className="w-4 h-4 border border-gray-400 bg-transparent rounded-full flex items-center justify-center peer-checked:border-black">
+                    <div className="w-2 h-2 bg-black rounded-full transition-all duration-200 transform scale-0 opacity-0 peer-checked:scale-100 peer-checked:opacity-100"></div>
+                  </div>
+                  <span className="text-xs font-light text-gray-300">
+                    Accept our
+                    <a className="ml-1 text-xs font-light text-gray-300 underline" href="/">
+                      Terms and Conditions
+                    </a>
+                  </span>
+                </label>
+              </div>
+
+              {/* Buttons */}
+              <div className="w-full flex flex-col gap-3 p-3">
+                <button className="bg-black text-sm text-white font-light text-center w-full py-3 rounded-full" type="submit">
+                  Sign Up
+                </button>
+
+                <div className="flex flex-col justify-center items-center text-center">
+                  <p className="text-xs font-light text-gray-300">Do you have an account?</p>
+                  <button
+                    className="text-xs font-light text-black"
+                    onClick={() => {
+                      setLoginStatus("login");
+                    }}
+                  >
+                    Sign in
+                  </button>
+                </div>
+              </div>
+            </div>
+          </div>
+        ) : loginStatus === "password" ? (
+          <div className="flex flex-col border border-gray-200 rounded-[32px] shadow-xl p-3">
+            {/* Header Row */}
+            <div className="flex justify-between items-center border-b border-gray-200 p-3 py-5">
+              {/* Icon and Title */}
+              <div className="flex justify-start items-center gap-4 w-full">
+                <UserEditIcon className={"w-6 h-6 fill-white"} />
+                <p className="text-2xl text-black font-semibold">Forget Password</p>
+              </div>
+            </div>
+          </div>
+        ) : null}
       </div>
     </div>
   );
