728480dc6e4fe5de9494d27dc3a22a7cfc888ef6
Efekan EFE <efekanefos@gmail.com>
2025-02-27 16:01:47 +0300

    local_amenities_gps

--- src/components/LocalAmenitiesGPS.jsx
+++ src/components/LocalAmenitiesGPS.jsx
@@ -8,17 +8,35 @@ import CarIcon from "../components/svg/CarIcon";
 import BcycleIcon from "../components/svg/BcycleIcon";
 import WalkIcon from "../components/svg/WalkIcon";
 import CloseIcon from "../components/newIcons/CloseIcon";
+import SendMessageIcon from "../components/svg/SendMessage";
+import ShareIcon from "../components/svg/ShareIcon";
+import MobileDownArrowIcon from "../components/newIcons/MobileDownArrowIcon";
+import LocationDownTriangle from "../components/svg/LocationDownTriangle";
 
 /* Images */
 import buildingCircleImage from "../components/svg/buildingCircleImage.png";
 import localBackground from "../assets/images/homepage/local_amenities_background.svg";
+import BoxIcon from "./svg/BoxIcon";
 
 function LocalAmenitiesGPS() {
   return (
     <div>
+      {/* Buttons */}
+      <div className="absolute top-5 right-0 hidden max-sm:flex justify-end items-center gap-2 mb-6 mr-12 max-md:mr-5">
+        <div className="hidden max-sm:flex justify-center items-center w-12 h-12 border border-gray-200 rounded-full p-3 bg-white">
+          <SendMessageIcon className={"w-6 h-6 fill-black translate-x-0.5"} />
+        </div>
+        <div className="hidden max-sm:flex justify-center items-center w-12 h-12 border border-gray-200 rounded-full p-3 bg-white">
+          <ShareIcon className={"w-5 h-5 fill-black "} />
+        </div>
+        <div className="w-12 h-12 flex justify-center items-center border border-gray-200 rounded-full p-3 bg-white">
+          <CloseIcon className={"w-3.5 h-3.5 fill-black "} />
+        </div>
+      </div>
+
       <div className="border-t border-gray-200 border-b">
         {/* Location Industries */}
-        <div className="w-full py-3 bg-white flex justify-center items-center z-10">
+        <div className="max-md:hidden w-full py-3 bg-white flex justify-center items-center z-10">
           {/* Buttons */}
           <div className="flex justify-center items-center gap-3">
             {/* Hospitality Button */}
@@ -49,7 +67,7 @@ function LocalAmenitiesGPS() {
 
         {/* Hospitality Column */}
 
-        <div className="absolute left-12 top-20 bg-white rounded-3xl min-w-72 shadow-lg p-6 h-4/5">
+        <div className="max-md:hidden absolute left-12 top-20 bg-white rounded-3xl min-w-72 shadow-lg p-6 h-4/5">
           {/* First Column */}
           <div className="flex justify-between items-center border-b border-gray-200 p-2.5 pl-3 mb-3">
             <div className="flex justify-start items-center gap-3">
@@ -84,83 +102,125 @@ function LocalAmenitiesGPS() {
           </ul>
         </div>
 
-        {/* Distance Bar */}
+        {/* Circled Reserve Icon with Border */}
+        <div className="absolute top-80 left-96 p-2 border-2 border-black rounded-full bg-white max-sm:left-[50%] max-sm:translate-x-[-50%]">
+          <HospitalityIcon className={"w-4 h-4 stroke-black stroke-[1.5px] fill-none"} />
+        </div>
 
-        <div className="fixed bottom-3 left-[50%] translate-x-[-50%] flex justify-center items-center bg-white rounded-full w-fit shadow-lg">
+        {/* Distance Bar */}
+        <div className="fixed bottom-6 left-1/2 translate-x-[-50%] flex justify-center items-center gap-2.5 bg-black rounded-full w-72 shadow-lg">
           {/* Distance Side */}
-          <div className="flex flex-col justify-center items-start pl-8 pr-5 border-r border-gray-200 min-h-16">
-            <p className="text-xs leading-4">Distance</p>
-            <p className="text-xl font-semibold leading-5">8.19 mi</p>
+          <div className="py-2.5 pl-3">
+            <p className="text-xs text-white font-light">8.3 miles</p>
+            <p className="text-xl font-semibold leading-5 text-white">9999 min</p>
           </div>
           {/* Vehicle Type Side */}
-          <div className="flex justify-start items-center gap-1 py-3 pr-4 pl-4">
-            <div className="relative flex flex-col justify-center items-center bg-middleMenuTextBlack rounded-full p-2 w-14 h-14">
-              <div className="max-w-14 max-h-14">
-                <CarIcon className={"w-5 h-5 stroke-white"} />
-              </div>
-              <p className="text-white font-medium text-xs w-max">32 min</p>
-              <span className="absolute -top-0.5 right-0 w-4 h-4 bg-orange-500 rounded-full border border-white"></span>
+          <div className="flex justify-start items-center gap-3 p-1 pl-0">
+            <div className="w-12 h-12 flex justify-center items-center rounded-full bg-white">
+              <CarIcon className={"w-6 h-6 stroke-black stroke-[1.5px] fill-none"} />
             </div>
 
-            <div className="flex flex-col justify-center items-center bg-gray-200 rounded-full p-2 w-14 h-14">
-              <div className="max-w-14 max-h-14">
-                <BcycleIcon className={"w-7 h-5 fill-current"} />
-              </div>
-              <p className="text-middleMenuTextBlack font-medium text-xs w-max">83 min</p>
+            <div className="w-12 h-12 flex justify-center items-center rounded-full bg-gray-300">
+              <BcycleIcon className={"w-7 h-6 fill-gray-400"} />
             </div>
 
-            <div className="flex flex-col justify-center items-center bg-gray-200 rounded-full p-2 w-14 h-14">
-              <div className="max-w-14 max-h-14">
-                <WalkIcon className={"w-4 h-5 fill-current"} />
-              </div>
-              <p className="text-middleMenuTextBlack font-medium text-xs w-max">188 min</p>
+            <div className="w-12 h-12 flex justify-center items-center rounded-full bg-gray-300">
+              <WalkIcon className={"w-6 h-6 fill-gray-400"} />
             </div>
           </div>
         </div>
 
         {/* Location Info Box */}
-        <div className="fixed top-[50%] left-[75%]">
-          <img className="absolute -top-8 -left-16 w-16 h-16 shadow-2xl rounded-full" src={buildingCircleImage} alt="buildingCircle" />
-          <div className="bg-middleMenuTextBlack p-2 rounded-full border-2 border-white w-fit absolute -top-12">{/* <HospitalityIcon className={"w-5 h-5 fill-white"} /> */}</div>
-          <div className="relative">
-            <div
-              className="bg-white w-fit rounded-3xl rounded-tl-none min-w-52 shadow-lg before:bg-white before:w-4 before:h-4 before:absolute
-        before:top-0 before:-left-4 before:windowArrow before:rounded-tl-md
-        "
-            >
-              {/* Location Name Row */}
-              <div className="flex flex-col items-start justify-start py-4 px-6 border-b border-gray-300">
-                <p className="text-xl font-semibold text-middleMenuTextBlack">Pafilia Plaza</p>
-                <p className="text-xs font-medium text-middleMenuTextBlack">2 Bedrooms</p>
-              </div>
-              {/* Location Price Row */}
-              <div className="flex items-center justify-center gap-3 py-4 px-6 border-b border-gray-300">
-                <div className="flex justify-center items-center gap-1">
-                  <div className="flex flex-col">
-                    <p className="text-[8px] text-middleMenuTextBlack font-medium leading-3">From</p>
-                    <p className="text-sm text-middleMenuTextBlack font-semibold leading-3">$25000</p>
-                  </div>
-                </div>
+        <div className="max-md:hidden absolute top-96 right-80 flex flex-col justify-center items-center">
+          <div className="flex flex-col justify-start items-center">
+            <div className="w-fit px-5 py-3 bg-gray-800 rounded-2xl flex justify-between items-center gap-5">
+              <div className="text-gray-100 text-base font-semibold">Aria Residences</div>
+              <MobileDownArrowIcon className={"w-4 h-4 fill-white"} />
+            </div>
+            <LocationDownTriangle className={"w-6 h-3 fill-gray-800"} />
+          </div>
+          {/* Circle with Building Image */}
+          <img className="w-20 h-20 shadow-lg rounded-full border-2 border-gray-100 z-10 max-md:hidden" src={buildingCircleImage} alt="buildingCircle" />
+        </div>
 
-                <div className="flex justify-start items-center gap-1">
-                  <div>
-                    <p className="text-[8px] text-middleMenuTextBlack font-medium leading-3">Sqm</p>
-                    <p className="text-sm text-middleMenuTextBlack font-semibold leading-3">150-225</p>
-                  </div>
-                </div>
+        {/* Tablet/Mobile Building Info Window */}
+        <div className="absolute bottom-80 max-md:bottom-96 right-32 flex flex-col justify-center items-center max-sm:left-[50%] max-sm:right-auto max-sm:translate-x-[-50%]">
+          <div className="min-w-56 w-fit  p-5 bg-white rounded-2xl flex flex-col justify-start items-start gap-3 shadow-lg">
+            {/* Aria Residences */}
+            <div className="w-full">
+              <div className="w-full h-6 flex justify-between items-center">
+                <p className="text-black text-base font-semibold">Aria Residences</p>
+                <CloseIcon className={"w-4 h-4 fill-black"} />
               </div>
-              {/* Location Button Row */}
-              <div className="py-4 px-5">
-                <button className="flex justify-center items-center gap-1 py-2 px-14 bg-middleMenuTextBlack rounded-2xl">
-                  {/* <BoxIcon className={"w-5 h-5 fill-current mt-1"} /> */}
-                  <p className="text-xs text-white font-medium">Portal</p>
-                </button>
+              <p className="text-black text-sm font-light">€17,555,000+VAT</p>
+            </div>
+            {/* Bedrooms */}
+            <div className="w-full flex justify-between items-center">
+              <div className="min-w-20">
+                <p className="text-black text-xs font-medium">Bedrooms</p>
+                <p className="text-black text-sm font-light">2-3</p>
+              </div>
+              <div className="min-w-20">
+                <p className="text-black text-xs font-medium">Sqm</p>
+                <p className="text-black text-sm font-light">150-225</p>
               </div>
             </div>
+            {/* Button */}
+            <div className="py-1.5 w-full bg-black rounded-full flex justify-center items-center gap-1.5">
+              <BoxIcon className={"w-3 h-3 stroke-white fill-none"} />
+              <p className="text-white text-xs font-light">Portal</p>
+            </div>
+          </div>
+          <LocationDownTriangle className={"w-6 h-3 fill-white"} />
+        </div>
+
+        {/* Tablet Industry Filter Buttons and Location List */}
+        <div className="hidden max-md:block py-6 px-16 absolute left-1/2 bottom-20 translate-x-[-50%] w-full max-sm:px-3 max-sm:py-6">
+          <div className="rounded-3xl   bg-white shadow-lg border border-gray-200">
+            {/* Buttons */}
+            <div className="flex justify-between items-center gap-3 py-3 px-16 shadow-lg w-full max-sm:max-w-[630px] max-sm:overflow-x-auto max-sm:justify-start max-sm:px-3">
+              <div className="py-1.5 px-3 flex justify-center items-center gap-1.5 rounded-full bg-gray-200 w-full">
+                <HospitalityIcon className={"w-5 h-5 stroke-gray-300 fill-none"} />
+                <p className="text-gray-400 text-xs font-light">Hospitality</p>
+              </div>
+              <div className="py-1.5 px-3 flex justify-center items-center gap-1.5 rounded-full bg-gray-200 w-full">
+                <EntertainmentIcon className={"w-5 h-5 stroke-black fill-none"} />
+                <p className="text-black text-xs font-light">Entertainment</p>
+              </div>
+              <div className="py-1.5 px-3 flex justify-center items-center gap-1.5 rounded-full bg-gray-200 w-full">
+                <HealthIcon className={"w-5 h-5 stroke-gray-300 fill-none"} />
+                <p className="text-gray-400 text-xs font-light">Health</p>
+              </div>
+              <div className="py-1.5 px-3 flex justify-center items-center gap-1.5 rounded-full bg-gray-200 w-full">
+                <ShopIcon className={"w-5 h-5  fill-gray-300"} />
+                <p className="text-gray-400 text-xs font-light">Shop</p>
+              </div>
+            </div>
+            {/* Location List */}
+            <ul className="grid grid-cols-2 gap-x-6 justify-between items-center pt-3 px-16 max-sm:grid-cols-1 max-sm:place-items-start max-sm:max-h-36 max-sm:overflow-y-auto max-sm:px-5">
+              <li className="py-3 px-6 bg-gray-200 rounded-full max-sm:min-w-60">
+                <p className="text-black text-sm font-light">Club V</p>
+              </li>
+              <li className="py-3 px-6 rounded-full cursor-pointer hover:bg-gray-200 max-sm:min-w-60 max-sm:w-fit">
+                <p className="text-black text-sm font-light">Family Entertainment Center</p>
+              </li>
+              <li className="py-3 px-6 rounded-full cursor-pointer hover:bg-gray-200 max-sm:min-w-60">
+                <p className="text-black text-sm font-light">Club C</p>
+              </li>
+              <li className="py-3 px-6 rounded-full cursor-pointer hover:bg-gray-200 max-sm:min-w-60">
+                <p className="text-black text-sm font-light">Cinema A</p>
+              </li>
+              <li className="py-3 px-6 rounded-full cursor-pointer hover:bg-gray-200 max-sm:min-w-60">
+                <p className="text-black text-sm font-light">Social Club</p>
+              </li>
+              <li className="py-3 px-6 rounded-full cursor-pointer hover:bg-gray-200 max-sm:min-w-60">
+                <p className="text-black text-sm font-light">Cinema B</p>
+              </li>
+            </ul>
           </div>
         </div>
       </div>
-      <img className="w-full h-full object-cover" src={localBackground} alt="localBackground" />
+      <img className="w-full h-screen object-cover" src={localBackground} alt="localBackground" />
     </div>
   );
 }
---
+++ src/components/svg/LocationDownTriangle.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LocationDownTriangle = ({ className }) => {
+  return (
+    <svg className={className} viewBox="0 0 24 13" xmlns="http://www.w3.org/2000/svg">
+      <path d="M7.05163 10.2836C9.43502 13.7594 14.565 13.7594 16.9484 10.2836L24 0H0L7.05163 10.2836Z" />
+    </svg>
+  );
+};
+
+export default LocationDownTriangle;
